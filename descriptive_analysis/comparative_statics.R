f8a2ee01eecfe5a40c2c562a1ab085a9ddc8f882
gustavo-tm <gtheilm@gmail.com>
2023-04-18 19:15:58 -0300

    fixes, comparative statics

---
+++ descriptive_analysis/comparative_statics.R
@@ -0,0 +1,56 @@
+library(ggthemes)
+library(ggplot2)
+windowsFonts(A = windowsFont("Times New Roman"))
+
+pontos <- seq(0, 1, 0.001)[-1]
+
+gg <- ggplot() +
+  ylim(c(0, 120)) +
+  theme_bw() +
+  theme(axis.text.y = element_blank(),
+        text = element_text(family = "A")) +
+  labs(y = "E(U)",
+       x = "Comparecimento")
+
+plot.1 <- gg +
+  geom_line(aes(pontos, 7 / pontos + 5)) +
+  geom_hline(yintercept = 15) +
+  geom_vline(xintercept = 7/(15-5), linetype = "dashed", alpha = .3) +
+  annotate("text", x=.25, y= 15+2^2, label="CMG") +
+  annotate("text", x=.25, y= 40+2^2, label="BMG") +
+  labs(title = "Mercado de votos")
+
+plot.2 <- gg +
+  geom_line(aes(pontos, 7 / pontos + 5)) +
+  geom_hline(yintercept = 15, linetype = "dashed") +
+  geom_hline(yintercept = 20) +
+  geom_vline(xintercept = 7/(15-5), linetype = "dashed", alpha = .3) +
+  geom_vline(xintercept = 7/(20-5), linetype = "dashed", alpha = .3) +
+  annotate("text", x=.25, y= 20+2^2, label="CMG'") +
+  annotate("text", x=.25, y= 40+2^2, label="BMG") +
+  labs(title = "Mercado de votos - Mudança I")
+
+plot.3 <- gg +
+  geom_line(aes(pontos, 7 / pontos + 5), linetype = "dashed") +
+  geom_line(aes(pontos, 9 / pontos + 5)) +
+  geom_hline(yintercept = 15) +
+  geom_vline(xintercept = 7/(15-5), linetype = "dashed", alpha = .3) +
+  geom_vline(xintercept = 9/(15-5), linetype = "dashed", alpha = .3) +
+  annotate("text", x=.25, y= 15+2^2, label="CMG") +
+  annotate("text", x=.25, y= 50+2^2, label="BMG'") +
+  labs(title = "Mercado de votos - Mudança II")
+
+plot.4 <- gg +
+  geom_line(aes(pontos, 7 / pontos + 5), linetype = "dashed") +
+  geom_line(aes(pontos, 7 / pontos + 7)) +
+  geom_hline(yintercept = 15) +
+  geom_vline(xintercept = 7/(15-5), linetype = "dashed", alpha = .3) +
+  geom_vline(xintercept = 7/(15-7), linetype = "dashed", alpha = .3) +
+  annotate("text", x=.25, y= 15+2^2, label="CMG") +
+  annotate("text", x=.25, y= 50+2^2, label="BMG'") +
+  labs(title = "Mercado de votos - Mudança III")
+
+ggsave("descriptive_analysis/mercado_votos.png", plot.1,  dpi = 600)
+ggsave("descriptive_analysis/mercado_votos_I.png", plot.2,  dpi = 600)
+ggsave("descriptive_analysis/mercado_votos_II.png", plot.3,  dpi = 600)
+ggsave("descriptive_analysis/mercado_votos_III.png", plot.4,  dpi = 600)
---
+++ descriptive_analysis/descriptive_analysis.R
@@ -0,0 +1,31 @@
+library(readr)
+library(tidyverse)
+library(geobr)
+library(ggplot2)
+library(sf)
+
+municipios <- read_municipality(code_muni = "all", year = 2018)
+
+df <- read_csv("data.csv") %>% 
+  select(id_municipio, abstencao, ano, turno) %>%
+  rename("code_muni" = "id_municipio")
+  
+data <- left_join(municipios, df, by = "code_muni")
+
+gg <- ggplot() +
+  geom_sf(data = data %>% filter(ano == 2022 & turno == 2), aes(fill = abstencao), color = NA) +
+  scale_fill_gradient2(midpoint = median(df$abstencao), low = "#5C5B60", mid = "#B3DBCF", high = "#D28673")+
+  theme_void()
+
+ggsave("desciptive_analysis/map.png", gg, dpi = 600, bg='transparent')
+
+gg <- ggplot(df, aes(x = factor(ano), y = abstencao)) +
+  geom_violin() +
+  geom_hline(yintercept = mean(na.omit(df$abstencao)), color = "black", linetype = "dashed") +
+  facet_wrap("turno") +
+  stat_summary(fun = "mean", geom = "crossbar", width = .5) +
+  theme_void()
+
+ggsave("desciptive_analysis/abstencao.png", gg, dpi = 600, bg='transparent')
+
+
